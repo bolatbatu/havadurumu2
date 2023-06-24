edc018a15d3a4eda946bcbaa6a9cba9388ab7873
Batuhan Bolat <125748985+bolatbatu@users.noreply.github.com>
2023-06-24 23:47:29 +0300

    Update

--- havadurumu.py
+++ havadurumu.py
@@ -1,11 +1,12 @@
 import requests
 import time
 
-def get_weather_data():
-    url = "https://api.openweathermap.org/geo/1.0/direct?q=Ankara&limit=5&appid=779c271986d91ab4f86362ce5d62e73c"
+def get_weather_data(city):
+    url = f"https://api.openweathermap.org/geo/1.0/direct?q={city}&limit=5&appid=779c271986d91ab4f86362ce5d62e73c"
     r = requests.get(url)
     data = r.json()
     return data
+
 
 def get_temperature(lat, lon):
     url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=779c271986d91ab4f86362ce5d62e73c&units=metric"
@@ -15,11 +16,13 @@ def get_temperature(lat, lon):
     return temperature
 
 while True:
-    weather_data = get_weather_data()
-    lat = weather_data[0]['lat']
-    lon = weather_data[0]['lon']
-    temp = get_temperature(lat, lon)
-    
-    print(f"Ankara'nın sıcaklığı: {temp}°C")
-    
+    city = input("Enter a city name (e.g., Ankara): ")
+    weather_data = get_weather_data(city)
+    if len(weather_data) > 0:
+        lat = weather_data[0]['lat']
+        lon = weather_data[0]['lon']
+        temp = get_temperature(lat, lon)
+        print(f"{city}'s temperature: {temp}°C")
+    else:
+        print("Invalid city name. Please try again.")
     time.sleep(0.30)
